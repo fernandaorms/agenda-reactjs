3dc3b116679c262453d065abda1132599de5067e
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-10-30 10:04:27 -0300

    add user photos

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,49 +1,157 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef  } from 'react';
 import { toast } from 'react-toastify';
 import { get } from 'lodash';
-import { FaFileImage } from 'react-icons/fa6';
-import { useNavigate } from 'react-router-dom';
+import { FaFileImage, FaTrashCan } from 'react-icons/fa6';
 import { useSelector } from 'react-redux';
 
 import axios from '../../services/axios';
 import Loader from '../../components/Loader';
-import { Container, Gallery, LoadPhoto, Photo } from './styled';
+import ConfirmationPopUp from '../../components/ConfirmationPopUp';
+import { Container, Gallery, Form, LoadPhoto, Photo, Buttons } from './styled';
+import { DangerButtonLight, PrimaryButton } from '../../styles/buttons';
 
 const Photos = () => {
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    const navigate = useNavigate();
 
     const [photos, setPhotos] = useState([]);
+    const [deletePhotoId, setDeletePhotoId] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [files, setFiles] = useState([]);
+    const [errors, setErrors] = useState([]);
+    const [showPopUp, setShowPopUp] = useState(false);
+    const fileInputRef = useRef(null);
+
+    const fetchPhotos = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.get('photos');
+
+            setPhotos(response.data);
+            setIsLoading(false);
+        } catch (err) {
+            const errors = get(err, 'response.data.errors', []);
+
+            if (errors.length > 0) errors.map((error) => toast.error(error));
+            else toast.error(err.message);
+
+            setIsLoading(false);
+        }
+    };
 
     useEffect(() => {
-        if(!isLoggedIn) return;
+        if (!isLoggedIn) return;
 
-        async function getData() {
-            try {
-                setIsLoading(true);
+        fetchPhotos();
+    }, [isLoggedIn]);
 
-                const response = await axios.get('photos');
+    const handleFileChange = (e) => {
+        const selectedFiles = Array.from(e.target.files);
+        setFiles(selectedFiles);
 
-                setPhotos(response.data);
-                setIsLoading(false);
-            } catch (err) {
-                const errors = get(err, 'response.data.errors', []);
+        setErrors([]);
+    };
 
-                if (errors.lenght) errors.map((error) => toast.error(error));
-                else toast.error(err.message);
+    const resetFiles = (e) => {
+        if(fileInputRef.current) {
+            fileInputRef.current.value = null;
+            setFiles([]);
+        }
+    }
 
-                setIsLoading(false);
+    const validateFormClient = () => {
+        let hasErrors = false;
 
-                navigate('/');
-            }
+        const newErrors = [];
+
+        if(files.length < 1 || files.length > 10) {
+            hasErrors = true;
+            newErrors.push('You must upload between 1 and 5 files.');
         }
 
-        getData();
-    }, [isLoggedIn, navigate]);
+        setErrors(newErrors);
+
+        return hasErrors;
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if( validateFormClient()) return;
+
+        const formData = new FormData();
+
+        files.forEach((file) => {
+            formData.append('photos', file);
+        });
+
+        setIsLoading(true);
+
+        try {
+            await axios.post('photos', formData);
+
+            toast.dismiss();
+            toast.success('Upload successful!');
+
+            setIsLoading(false);
+            await fetchPhotos();
+            resetFiles();
+        } catch (err) {
+            const errors = get(err, 'response.data.errors', []);
+
+            if (errors.length > 0) errors.map((error) => toast.error(error));
+            else toast.error(err.message);
+
+            setIsLoading(false);
+        }
+    }
+
+    const handleDeleteAsk = async (e, photoId) => {
+        e.preventDefault();
+        setDeletePhotoId(photoId);
+        setShowPopUp(true);
+    }
+
+    const handleConfirm = async () => {
+        setShowPopUp(false);
+        setIsLoading(true);
+        
+        try {
+            await axios.delete(`photos/${deletePhotoId}`);
+
+            setIsLoading(false);
+
+            toast.dismiss();
+            toast.success('Delete successful!');
+
+            await fetchPhotos();
+            resetFiles();
+        } catch(err) {
+            const errors = get(err, 'response.data.errors', []);
+
+            if (errors.length > 0) errors.map((error) => toast.error(error));
+            else toast.error(err.message);
+
+            setIsLoading(false);
+        }
+
+        setDeletePhotoId(null);
+    };
+
+    const handleCancel = () => {
+        setShowPopUp(false);
+    };
 
     return (
         <main>
+            { showPopUp && (
+                <ConfirmationPopUp
+                    onConfirm={handleConfirm}
+                    onCancel={handleCancel}
+                    actionName='Delete Photo'
+                />
+            )}
+
             <section className='main'>
                 <div className='container'>
                     <Container className='loader-container'>
@@ -53,17 +161,58 @@ const Photos = () => {
                             <h1>Photos</h1>
                         </div>
 
-                        <Gallery>
-                            <LoadPhoto>
-                                <FaFileImage />
-                            </LoadPhoto>
+                        <Form onSubmit={handleSubmit}>
+                            {errors.length > 0 && (
+                                <div className='errors'>
+                                    {errors.map((error, index) => (
+                                        <span key={index} className='error'>{error}</span>
+                                    ))}
+                                </div>
+                            )}
 
-                            {photos.map((photo) => (
-                                <Photo key={photo.id}>
-                                    <img src={photo.url} alt={photo.originalname} />
-                                </Photo>
-                            ))}
-                        </Gallery>
+                            {files.length > 0 && (
+                                <>
+                                    <div className='files-list'>
+                                        <div className='files'>
+                                            <span>Uploaded {files.length} files:</span>
+                                        </div>
+
+                                        <div className='list'>
+                                            {files.map((file, index) => (
+                                                <span key={index}>
+                                                    {file.name}{index < files.length - 1 && (';')}
+                                                </span>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <Buttons>
+                                        <PrimaryButton type='submit'>Save</PrimaryButton>
+                                        <DangerButtonLight onClick={resetFiles}>Cancel</DangerButtonLight>
+                                    </Buttons>
+                                </>
+                            )}
+                            
+                            <Gallery>
+                                <div className='field line'>
+                                    <LoadPhoto htmlFor='files'>
+                                        <FaFileImage />
+                                        <span>Click here to upload</span>
+                                    </LoadPhoto>
+
+                                    <input type='file' id='files' ref={fileInputRef} multiple accept='image/*' onChange={handleFileChange} />
+                                </div>
+
+
+                                {photos.map((photo) => (
+                                    <Photo key={photo.id} className={photo.id} onClick={(e) => handleDeleteAsk(e, photo.id)}>
+                                        <img src={photo.url} alt={photo.filename} />
+                                        
+                                        <FaTrashCan className='delete' />
+                                    </Photo>
+                                ))}
+                            </Gallery>
+                        </Form>
                     </Container>
                 </div>
             </section>
--- src/pages/Photos/styled.js
+++ src/pages/Photos/styled.js
@@ -17,22 +17,43 @@ export const Container = styled.div`
 
         margin-bottom: 40px;
     }
+
+    .files-list {
+        .files {
+            font-weight: 600;
+            margin-bottom: 8px;
+        }
+
+        .list {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 4px 12px;
+
+            span {
+                display: block;
+                white-space: nowrap;
+            }
+        }
+    }
+`
+
+export const Buttons = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
+    margin: 32px 0;
+
+    @media screen and (max-width: 767px) {
+        grid-template-columns: 1fr;
+        gap: 12px;
+        margin: 24px 0;
+    }
 `
 
 export const Gallery = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     gap: 24px;
-
-    > div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        aspect-ratio: 1;
-        border-radius: 8px;
-        overflow: hidden;
-        border: 2px solid ${colors.border_color};
-    }
 
     @media screen and (max-width: 1199px) {
         & {
@@ -53,10 +74,42 @@ export const Gallery = styled.div`
     }
 `;
 
+export const Form = styled.form`
+    .field {
+        position: relative;
+    }
 
-export const LoadPhoto = styled.div`
-    
-    font-size: 32px;
+    input {
+        display: none;
+    }
+
+    .errors {
+        margin-bottom: 16px;
+    }
+
+    .error {
+        display: block;
+        font-size: 14px;
+        font-weight: 500;
+        color: ${colors.danger_color};
+        line-height: 135%;
+        margin-bottom: 8px;
+    }
+`
+
+export const LoadPhoto = styled.label`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    padding: 16px;
+    aspect-ratio: 1;
+    border-radius: 8px;
+    overflow: hidden;
+    text-align: center;
+    border: 2px solid ${colors.border_color};
+    font-size: 24px;
     color: ${colors.border_color};
     transition: .3s ease;
     cursor: pointer;
@@ -65,12 +118,100 @@ export const LoadPhoto = styled.div`
         border-color: ${colors.primary_color};
         color: ${colors.primary_color};
     }
+
+    span{
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+    @media screen and (max-width: 575px) {
+        border-color: ${colors.primary_color};
+        color: ${colors.primary_color};
+
+        span{
+            font-size: 16px;
+            line-height: 125%;
+        }
+    }
 `;
 
-
 export const Photo = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    aspect-ratio: 1;
+    border-radius: 8px;
+    overflow: hidden;
+    border: 2px solid ${colors.border_color};
+    cursor: pointer;
+    transition: .3s ease;
+
     img {
         height: 100%;
         width: 100%;
+    }
+
+    .delete {
+        position: absolute;
+        color: ${colors.danger_color};
+        transition: .3s ease;
+    }
+
+    &:hover {
+        border-color: ${colors.danger_color};
+    }
+
+    @media screen and (min-width: 768px){
+        .delete {
+            top: calc(50% - 12px);
+            left: calc(50% - 12px);
+            font-size: 28px;
+            opacity: 0;
+            visibility: hidden;
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            background: rgba(255, 255, 255, 0);
+            transition: .3s ease;
+        }
+
+        &:hover {
+            .delete {
+                opacity: 1;
+                visibility: visible;
+            }
+
+            &::before{
+                background-color: rgba(255, 255, 255, 0.85);
+            }
+        }
+    }
+
+    @media screen and (max-width: 767px) {
+        .delete {
+            font-size: 18px;
+            bottom: 12px;
+            color: #FFF;
+            left: calc(50% - 9px);
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: calc(50% - 20px);
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            background: ${colors.danger_color};
+            transition: .3s ease;
+        }
     }
 `;
